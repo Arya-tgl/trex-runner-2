28d357b9ae1ce704bd57357cdfd6d439e13eb46e
Arya-tgl <75373154+Arya-tgl@users.noreply.github.com>
2020-12-09 17:39:02 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+
 var PLAY = 1;
 var END = 0;
 var gameState = PLAY;
@@ -5,15 +6,12 @@ var gameState = PLAY;
 var trex, trex_running, trex_collided;
 var ground, invisibleGround, groundImage;
 
-
 var cloudsGroup, cloudImage;
 var obstaclesGroup, obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obstacle6;
 
 var score;
 var gameOverImg,restartImg
 var jumpSound , checkPointSound, dieSound
-localStorage["HighestScore"]=0
-
 
 function preload(){
   trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
@@ -39,33 +37,31 @@ function preload(){
 }
 
 function setup() {
-  createCanvas(600, 200);
+  createCanvas(windowWidth, windowHeight);
 
-  var message = "This is a message";
- console.log(message)
   
-  trex = createSprite(50,160,20,50);
+  trex = createSprite(50,height-220,20,50);
   trex.addAnimation("running", trex_running);
   trex.addAnimation("collided", trex_collided);
   
 
   trex.scale = 0.5;
   
-  ground = createSprite(200,180,400,20);
+  ground = createSprite(width/2,height-200,width,2);
   ground.addImage("ground",groundImage);
   ground.x = ground.width /2;
   
-  gameOver = createSprite(300,100);
+  gameOver = createSprite(width/2,height/2-50);
   gameOver.addImage(gameOverImg);
   
-  restart = createSprite(300,140);
+  restart = createSprite(width/2,height/2);
   restart.addImage(restartImg);
   
  
-  gameOver.scale = 0.5;
-  restart.scale = 0.5;
+  //gameOver.scale = 0.5;
+  //restart.scale = 0.5;
   
-  invisibleGround = createSprite(200,190,400,10);
+  invisibleGround = createSprite(width/2,height-190,width,10);
   invisibleGround.visible = false;
   
   //create Obstacle and Cloud Groups
@@ -83,9 +79,10 @@ function setup() {
 function draw() {
   
   background(180);
-  text("high score: "+localStorage["HighestScore"],400,50)
   //displaying score
-  text("Score: "+ score, 500,50);
+  textStyle(BOLD)
+  textSize(25)
+  text("Score: "+ score,width-150,50);
   
   
   if(gameState === PLAY){
@@ -106,9 +103,10 @@ function draw() {
     }
     
     //jump when the space key is pressed
-    if(keyDown("space")&& trex.y >= 150) {
+    if((keyDown("space") || touches.length>0) && trex.y >= windowHeight-50) {
         trex.velocityY = -12;
         jumpSound.play();
+      touches=[]
     }
     
     //add gravity
@@ -153,8 +151,9 @@ function draw() {
   //stop trex from falling down
   trex.collide(invisibleGround);
   
-  if(mousePressedOver(restart)) {
+  if(mousePressedOver(restart) || touches.length>0) {
       reset();
+    touches=[]
     }
 
 
@@ -166,9 +165,6 @@ function reset(){
   obstaclesGroup.destroyEach()
   cloudsGroup.destroyEach()
   trex.changeAnimation("running",trex_running)
-  if(score>localStorage["HighestScore"]){
-    localStorage["HighestScore"]=score
-  }
   score=0
   
   
@@ -177,7 +173,7 @@ function reset(){
 
 function spawnObstacles(){
  if (frameCount % 60 === 0){
-   var obstacle = createSprite(600,165,10,40);
+   var obstacle = createSprite(width,height-210,10,40);
    obstacle.velocityX = -(6 + score/100);
    
     //generate random obstacles
@@ -210,8 +206,8 @@ function spawnObstacles(){
 function spawnClouds() {
   //write code here to spawn the clouds
   if (frameCount % 60 === 0) {
-    var cloud = createSprite(600,120,40,10);
-    cloud.y = Math.round(random(80,120));
+    var cloud = createSprite(width,height-300,40,10);
+    cloud.y = Math.round(random(height-300,height-500));
     cloud.addImage(cloudImage);
     cloud.scale = 0.5;
     cloud.velocityX = -3;
@@ -227,4 +223,6 @@ function spawnClouds() {
     cloudsGroup.add(cloud);
   }
 }
+
+
 
